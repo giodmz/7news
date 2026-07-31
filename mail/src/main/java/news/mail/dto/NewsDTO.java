4b101987149be1f9d14a5d5e6862a70650042edc
Giovane Damazio <giovxnedamazio@gmail.com>
2026-07-31 16:39:54 -0300

    update: validation

--- mail/src/main/java/news/mail/dto/NewsDTO.java
+++ mail/src/main/java/news/mail/dto/NewsDTO.java
@@ -1,5 +1,6 @@
 package news.mail.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,12 @@ import java.util.UUID;
 @ToString
 public class NewsDTO {
     UUID uuid;
+
+    @NotBlank(message = "Title is required")
     String title;
+
+    @NotBlank(message = "Content is required")
     String content;
+
     String imageUrl;
 }
--- notice/src/main/java/notice/dto/NewsDTO.java
+++ notice/src/main/java/notice/dto/NewsDTO.java
@@ -1,5 +1,6 @@
 package notice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,13 @@ import java.util.UUID;
 public class NewsDTO {
 
     UUID uuid;
+
+    @NotBlank(message = "Title is required")
     String title;
+
+    @NotBlank(message = "Content is required")
     String content;
+
     String imageUrl;
 
 }
